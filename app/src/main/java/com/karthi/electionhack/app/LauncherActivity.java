166a716e87f4347d3a3ff6ff7667d0ecd3509fb5
karthik <intrepidkarthi@gmail.com>
2014-03-30 00:32:11 +0530

    Adding api call async task

--- app/src/main/java/com/karthi/electionhack/app/LauncherActivity.java
+++ app/src/main/java/com/karthi/electionhack/app/LauncherActivity.java
@@ -1,22 +1,100 @@
 package com.karthi.electionhack.app;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.karthi.electionhack.utils.AppConstants;
+import com.karthi.electionhack.utils.GPSTracker;
+import com.karthi.electionhack.utils.ServiceHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class LauncherActivity extends Activity {
+
+    //Variable declarations
+    String deviceId;
+    // GPSTracker class
+    GPSTracker gps;
+
+    ProgressDialog pDialog;
+    double latitude;
+    double longitude;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+        getDeviceId();
+
+
+        // create class object
+        gps = new GPSTracker(LauncherActivity.this);
+
+        // check if GPS enabled
+        if(gps.canGetLocation()){
+            latitude = gps.getLatitude();
+            longitude = gps.getLongitude();
+        }else{
+
+            // can't get location
+            // GPS or Network is not enabled
+            // Ask user to enable GPS/network in settings
+            gps.showSettingsAlert();
+        }
+
+        new MakeAPICall().execute("createMethod");
+
+
+
     }
 
+    private final LocationListener locationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+            //System.out.println("network "+"onstatus changed"+ s);
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+            //System.out.println("network "+"on provider enabled"+ s);
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+            //System.out.println("network "+"on provider disabled"+ s);
+        }
+    };
+
+
+    public String getDeviceId() {
+        String deviceId;
+        try {
+            deviceId = Settings.System.getString(getContentResolver(),
+                    Settings.System.ANDROID_ID);
+        } catch (Exception e) {
+            return null;
+        }
+        return deviceId;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.launcher, menu);
         return true;
@@ -33,5 +111,62 @@ public class LauncherActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    /**
+     * Async task class to get json by making HTTP call
+     * */
+    private class MakeAPICall extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(LauncherActivity.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected Void doInBackground(String... arg0) {
+            // Creating service handler class instance
+            ServiceHandler sh = new ServiceHandler();
+
+            // Making a request to url and getting response
+            String jsonStr = sh.makeServiceCall(AppConstants.SERVER_BASE_URL, ServiceHandler.GET);
+
+            Log.d("Response: ", "> " + jsonStr);
+
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Log.e("ServiceHandler", "Couldn't get any data from the url");
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+            /**
+             * Updating parsed JSON data into ListView
+             * */
+        }
+
+    }
+
+
 
 }
